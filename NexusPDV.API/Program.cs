dae896eec905ad9f3af2bfb795d70658eaff09d1
JulioNogueira99 <julio.nogueira561@gmail.com>
2026-01-07 17:16:28 -0300

    chore: Dockerização completa da aplicação (API + SQL Server)

--- NexusPDV.API/Program.cs
+++ NexusPDV.API/Program.cs
@@ -109,4 +109,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocorreu um erro ao migrar o banco de dados.");
+    }
+}
+
 app.Run();
