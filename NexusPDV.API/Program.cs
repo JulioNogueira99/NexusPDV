63a372c0095ef3bb11e857f8cf38e061ed80da9e
JulioNogueira99 <julio.nogueira561@gmail.com>
2026-01-07 17:38:28 -0300

    chore: Habilita CORS para permitir conexão com Frontend externo

--- NexusPDV.API/Program.cs
+++ NexusPDV.API/Program.cs
@@ -90,6 +90,16 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin() 
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -102,6 +112,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthentication();
 
