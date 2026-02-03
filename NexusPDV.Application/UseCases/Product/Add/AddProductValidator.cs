fc89c667785df3d29909011fdadeb3a002dbb425
JulioNogueira99 <julio.nogueira561@gmail.com>
2026-02-03 16:52:30 -0300

    feat: Validações de FluentValidation para produtos

--- NexusPDV.Application/UseCases/Product/Add/AddProductValidator.cs
+++ NexusPDV.Application/UseCases/Product/Add/AddProductValidator.cs
@@ -1,13 +1,22 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NexusPDV.Application.UseCases.Product.Add
 {
     public class AddProductValidator : AbstractValidator<AddProductCommand>
     {
+        public AddProductValidator()
+        {
+            RuleFor(x => x.Title.Trim())
+                .NotEmpty()
+                .WithMessage("O titulo do produto não pode ser vazio.");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0)
+                .WithMessage("O preço deve ser maior que zero");
+
+            RuleFor(x => x.StockQuantity)
+                .GreaterThan(0)
+                .WithMessage("Estoque inicial não pode ser negativo");
+        }
     }
 }
